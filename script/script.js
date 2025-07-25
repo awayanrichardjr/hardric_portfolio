5a51abec7a7bcc1c2e9e12e70d3b4f4b21a3fb9e
Richard Awayan <awayanrichardjr@gmail.com>
2025-07-26 00:44:45 +0800

    07-25

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// contact
+let github = "https://github.com/awayanrichardjr",
+  facebook = "https://www.facebook.com/awayanrichardjr",
+  instagram = "https://www.instagram.com/awayanrichardjr/",
+  linkedin = "https://www.linkedin.com/in/richard-dumael-awayan-jr-b256502b8/";
+
+// download cv
