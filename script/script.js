2cf3821cf94b7c19c7a687b2d9c1e0b0f19c0b8f
Richard Awayan <awayanrichardjr@gmail.com>
2025-07-27 16:11:00 +0800

    07-27

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 let github = "https://github.com/awayanrichardjr",
   facebook = "https://www.facebook.com/awayanrichardjr",
   instagram = "https://www.instagram.com/awayanrichardjr/",
-  linkedin = "https://www.linkedin.com/in/richard-dumael-awayan-jr-b256502b8/";
+  linkedin = "https://www.linkedin.com/in/richard-dumael-awayan-jr-b256502b8/",
+  map = "https://maps.app.goo.gl/7Gn7dnBTyT9a7YTu9";
 
 // download cv
